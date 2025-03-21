39f517ab493809ce21f75121dec983620ebc90b0
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2025-03-21 16:13:16 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let totalPrice = days * 40;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const LONG_TERM = 7;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  const MEDIUM_TERM = 3;
 
-  if (days >= 7) {
-    totalPrice -= 50;
-  } else if (days >= 3) {
-    totalPrice -= 20;
+  const totalPrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return totalPrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MEDIUM_TERM) {
+    return totalPrice - MEDIUM_TERM_DISCOUNT;
   }
 
   return totalPrice;
